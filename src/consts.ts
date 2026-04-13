7dcfb2e33d66eb9cdfd3441affb90e1f45718348
PrimaryC <byunggi.choi@gmail.com>
2026-04-13 10:35:30 +0900

    feat: add constants file for site configuration

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Director C';
+export const SITE_DESCRIPTION = 'Director C의 개발 및 서브컬처 일상 블로그';
